c63d23772279cc9cb3c0cb6bf7cbdd718d5e3752
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-06-10 18:25:30 +0200

    Changed keybindings for tmux

--- scripts/tmux-switcher.sh
+++ scripts/tmux-switcher.sh
@@ -5,7 +5,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~ ~/school ~/code -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~ ~/School ~/Code -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
